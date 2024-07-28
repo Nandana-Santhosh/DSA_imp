8374a201ab8ebaa6b88c1563cb2f5e07db10def8
Nandana Santhosh <nandanapsmail@gmail.com>
2024-07-28 14:34:29 +0530

    5 approaches to reverse a string

--- Reverse string in Python.py
+++ Reverse string in Python.py
@@ -16,3 +16,22 @@ def reverse(s):
         return reverse(s[1:])+s[0]
 s="APPLE"
 print(reverse(s))
+
+
+#3rd approach 
+def reverse(string):
+    string = string[::-1]
+    return string
+
+#4th approach
+
+reverse=''.join(reversed(s))
+
+
+#5th approach
+
+def reverse(string):
+    string = [string[i] for i in range(len(string)-1, -1, -1)]
+    return "".join(string)
+
+s = "Geeksforgeeks"
