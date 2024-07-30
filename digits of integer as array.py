abd49488346c808aab175eab539ed70532832391
Nandana Santhosh <nandanapsmail@gmail.com>
2024-07-30 15:42:22 +0530

    2

---
+++ digits of integer as array.py
@@ -0,0 +1,5 @@
+'''print digits of an integer as array'''
+number=123445667789
+
+array = [int(x) for x in str(number)]
+print(array)
---
+++ sort array of integer.py
@@ -0,0 +1,5 @@
+'''Sort the array of integer'''
+
+li=[1,6,2,8,9,2,0,2,1]
+new=sorted(li)
+print(new)
